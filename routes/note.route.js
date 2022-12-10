59a31cfd992f415f49c809a0e20e2f92a0e193bd
Param1Raj <pr6587424@gmail.com>
2022-12-10 16:36:08 +0530

    signing up

--- routes/note.route.js
+++ routes/note.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const NoteRouter = express.Router();
+const {NotesModel} = require('../model/notes.module')
 
 NoteRouter.get("/",(req,res)=>{
     res.send("Notes");
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,17 @@
 const express = require("express");
-
 const UserRouter = express.Router();
+const {UserModel} = require("../model/user.model")
 
 UserRouter.post('/signup',async(req,res)=>{
     let payload = req.body;
-    console.log(payload);
+    try {
+        let data = await UserModel.find({email});
+        if(data.length <= 0){
+            
+        }
+    } catch (error) {
+        console.log(error);
+    }
     res.send("Signing");
 })
 
