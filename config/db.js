6562457abc4ecbb678bada04d45bb48147c14988
Param Raj <107528212+Param1raj@users.noreply.github.com>
2022-12-10 17:14:52 +0530

    Delete db.js

--- config/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-const connection = mongoose.connect('mongodb+srv://paramraj:charu@cluster0.f8qnm6n.mongodb.net/Note_app?retryWrites=true&w=majority')
-
-module.exports = {connection};
