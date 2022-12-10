03a493b0dec98e51f9d6957e87a4eecd697a0d73
Param1Raj <pr6587424@gmail.com>
2022-12-10 15:40:11 +0530

    Commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hellow World");
+const express = require("express");
+const app = express();
+require('dotenv').config();
+// console.log(process.env);
+
+app.get("/",(req,res)=>{
+    res.send("Hellow")
+})
+
+app.listen(Number(process.env.PORT),()=>{
+    console.log(`Server is running on the port ${process.env.PORT}`)
+});
